8c5ea74ee13164255f926d66901555cbbe9bafad
Philip Ainza <philainza@gmail.com>
2016-04-05 10:13:42 -0700

    Add index route, controller, and view

---
+++ spec/features/list_projects_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "Viewing the list of projects" do 
+
+  it "shows three project names" do 
+    visit projects_url
+
+    expect(page).to have_text("3 Projects")
+    expect(page).to have_text("Project 1")
+    expect(page).to have_text("Project 2")
+    expect(page).to have_text("Project 3")
+  end
+
+end
