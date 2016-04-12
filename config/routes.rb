08ad6e3a23d6f6e2919b89ae7051034fec7592dc
Philip Ainza <philainza@gmail.com>
2016-04-12 11:12:23 -0700

    Add form to edit project details

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "projects#index"
   get "projects" => "projects#index" 
   get "projects/:id" => "projects#show", as: "project"
+  get "projects/:id/edit" => "projects#edit", as: "edit_project"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Project.create!([
+{
+  name: "Jistlist",
+  description: "A social network and meetup app for jiu-jitsu practitioners",
+  target_pledge_amount: 100000.00,
+  pledging_ends_on: 40.days.from_now,
+  website: "http://www.jitslist.com"
+},
+{
+  name: "Plutocracy",
+  description: "Compete against your peers and win real money in this fantasy sports style mock trading app.",
+  target_pledge_amount: 250000.00,
+  pledging_ends_on: 32.days.from_now,
+  website: "http://www.plutocracy.com"
+},
+{
+  name: "Paper Route",
+  description: "A modern 8-bit style remake of the classic video game, 'Paper Boy'.",
+  target_pledge_amount: 300000.00,
+  pledging_ends_on: 1.day.from_now,
+  website: "http://www.paperroute.com"
+}
+])
