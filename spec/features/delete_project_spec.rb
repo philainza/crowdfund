c298faa839e121d936f965126574a63c3ced4d93
Philip Ainza <philainza@gmail.com>
2016-04-15 09:35:59 -0700

    Fix delete project spec

--- spec/features/delete_project_spec.rb
+++ spec/features/delete_project_spec.rb
@@ -7,8 +7,10 @@ describe "Deleting a project" do
 
     visit project_path(project)
 
+    click_link "Delete"
+
     expect(current_path).to eq(projects_path)
-    expect(page).not_to have_text(project.title)
+    expect(page).not_to have_text(project.name)
   end
 
 end
